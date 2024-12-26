2168dc1e5004a0b2f21bb60e0182f8cf07944563
Kavinesh Reddy <kavineshreddy@Kavineshs-MacBook-Air.local>
2024-12-27 03:04:02 +0800

    second commit

---
+++ findaverage.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter 3 number to calculate average :"<<endl;
+    cin>>a>>b>>c;
+    double avg=(a+b+c)/3;
+    cout<<"Average is "<<avg<<endl;
+}
